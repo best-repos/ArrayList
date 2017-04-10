78a87b3a3dacfb5c36b17f05837c222d0b10acd3
liao jia bing <admin@w3c.top>
2017-04-10 09:46:07 +0800

    admin@w3c.top
    ArrayList Collection

---
+++ ArrayList.js
@@ -0,0 +1,93 @@
+class ArrayList{
+	/**
+	 * 构造函数,允许传入一个数组
+	 * @param  {[type]} array [description]
+	 * @return {[type]}       [description]
+	 */
+	constructor(array = []){
+		this.arr = array;
+	}
+	/**
+	 * 添加单个元素
+	 * @param {[type]} item [description]
+	 */
+	add(item){
+		this.arr.push(item);
+	}
+	/**
+	 * 添加多个元素
+	 */
+	addAll(...items){
+		items.forEach(e => this.arr.push(e));
+	}
+	/**
+	 * 遍历集合
+	 */
+	forEach(callback){
+		this.arr.forEach(e => callback(e));
+	}
+	/**
+	 * 移除所有元素
+	 * @return {[type]} [description]
+	 */
+	clear(){
+		this.arr.length = 0;
+	}
+	/**
+	 * 深克隆一个集合
+	 */
+	clone(){
+		let temp = new ArrayList();
+		this.arr.forEach(e => temp.add(e));
+		return temp;
+	}
+	/**
+	 * 检查集合中是否包含指定元素
+	 */
+	contains(item){
+		for(let i of this.arr){
+			if(i === item) return true;
+		}
+		return false;
+	}
+	/**
+	 * 获取指定位置上的元素
+	 */
+	get(index){
+		return this.arr[index];
+	}
+	/**
+	 * 返回此列表中首次出现的指定元素的索引，或如果此列表不包含元素，则返回 -1
+	 */
+	indexOf(item){
+		for(let i=0,len=this.arr.length;i<len;i++){
+			if(this.arr[i] === item) return i;
+		}
+		return -1;
+	}
+	/**
+	 * 判断集合是否为空
+	 */
+	isEmpty(){
+		return this.arr.length == 0;
+	}
+	/**
+	 * 移除此列表中指定位置上的元素
+	 */
+	remove(item){
+		for(let i=0,len=this.arr.length;i<len;i++){
+			if(item === this.arr[i]){
+				this.arr.splice(i,1);
+				return;
+			}
+		}
+	}
+	/**
+	 * 用指定的元素替代此列表中指定位置上的元素
+	 */
+	set(index,item){
+		if(index > this.arr.length) return;
+		this.arr[index] = item;
+	}
+}
+module.exports = ArrayList;
--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# ArrayList
-ArrayList集合工具类
+ArrayList API:
+
+下载<br>
+	npm install arraylist
+
+>引用包
+```javascript
+	let ArrayList = require("arraylist");
+```
+
+>创建一个ArrayList对象
+```javascript
+	let list = new ArrayList();
+```
+
+>添加一个元素
+```javascript
+	list.add(item);
+```
+
+>添加多个元素
+```javascript
+	list.add(item1,item2,item3...);
+```
+
+>删除指定元素
+```javascript
+	list.remove(item);
+```
+
+>清空集合元素
+```javascript
+	list.clear();
+```
+
+>深克隆一个集合
+```javascript
+	let list2 = list.clone();
+```
+
+>检查集合中是否包含指定元素
+```javascript
+	let bool = list.contains(item);
+```
+
+>获取指定位置上的元素
+```javascript
+	let temp = list.get(index);
+```
+
+>返回此列表中首次出现的指定元素的索引，或如果此列表不包含元素，则返回 -1
+```javascript
+	let index = list.indexOf(item);
+```
+
+>判断集合是否为空
+```javascript
+	let bool = list.isEmpty();
+```
+
+>用指定的元素替代此列表中指定位置上的元素
+```javascript
+	list.set(index,item);
+```
+
+关于作者:<br>
+	email:admin@w3c.top
